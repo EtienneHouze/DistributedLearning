458bca524c6732de0585da3e165b55b254a1b4fb
Etienne Houze <Etienne.Houze@polytechnique.edu>
2017-06-06 10:58:43 +0200

    Back to old loss

--- DistributedLearning/src/CityScapeModel.py
+++ DistributedLearning/src/CityScapeModel.py
@@ -252,7 +252,7 @@ class CityScapeModel:
             else:
                 metrics.append(met)
         self.model.compile(optimizer=self.prop_dict['opt'],
-                           loss=Metrics.weighted_loss_18,
+                           loss=self.prop_dict['loss'],
                            metrics=metrics,
                            sample_weight_mode=self.prop_dict['w_mode'])
         if 'learning_rate' in self.prop_dict.keys():
